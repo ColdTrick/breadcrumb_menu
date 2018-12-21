4514a85b3df771970eec9a121fb097f3ed5e2e0b
Jerome Bakker <jeabakker@coldtrick.com>
2018-12-21 14:00:22 +0100

    chore: added Dutch translation

---
+++ languages/nl.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * This file was created by Translation Editor v7.0-beta
+ * On 2018-12-21 13:59
+ */
+
+return array (
+  'breadcrumb_menu:settings:move_owner_block' => 'Verplaats eigenaar menu items naar het kruimelpad',
+  'breadcrumb_menu:settings:move_owner_block:help' => 'Indien ingeschakeld zal dit de menu items van de eigenaar verplaatsen van de zijbalk naar een dropdown in het kruimelpad.',
+  'breadcrumb_menu:settings:remove_last_empty_item' => 'Verwijder het laatste item uit het kruimelpad indien dit geen link is',
+  'breadcrumb_menu:settings:remove_last_empty_item:help' => 'Indien ingeschakeld zal dit het laatste menu item uit het kruimelpad verwijderen indien dit geen link is.',
+  'breadcrumb_menu:settings:remove_last_self_item' => 'Verwijder het laatste item uit het kruimelpad indien dit verwijst naar de huidige pagina',
+  'breadcrumb_menu:settings:remove_last_self_item:help' => 'Indien ingeschakeld zal het laatste menu item uit het kruimelpad worden verwijderd indien dit een link is naar de huidige pagina.',
+);
